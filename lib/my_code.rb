88332f8be03344f3f2576900a266c4e5ab61506b
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-09-30 19:35:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -89,3 +89,16 @@ def reduce_to_all_true2(array)
     end 
     true 
 end 
+
+def reduce_to_any_true(array)
+  i = 0 
+  
+  while i < array.length do 
+      if array[i] == true 
+        return true 
+      end 
+    i += 1 
+  end 
+  
+  false 
+end 
