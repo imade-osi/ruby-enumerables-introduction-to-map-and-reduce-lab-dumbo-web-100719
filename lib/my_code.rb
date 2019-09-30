cbbf39cb61a572927c2417f039f5cc5662a80e9a
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-09-30 19:08:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,3 +16,15 @@ end
 def map_to_no_change(array)
   array
 end 
+
+def map_to_double(array) 
+  arr = [] 
+  i = 0 
+  
+  while array[i] do 
+      arr << array[i] * 2 
+      i += 1 
+  end 
+  
+  arr
+end 
